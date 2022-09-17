cc88e407f7d8e49546ac193b0df112c408ff8b9f
Ivan Sidorenko <cidor.0712@gmail.com>
2022-09-17 14:03:29 +0500

    Fix Task13

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -1,14 +1,10 @@
 ﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 
-Console.Write ("Введите трехзначное число: ");
+Console.Write ("Введите любое число: ");
 int a = Convert.ToInt32(Console.ReadLine());
 string b = Convert.ToString(a);
 int Length = b.Length;
-if (Length >= 4) 
-{
-    Console.Write ("Введенно неверное значение");
-}
-else if (Length > 2)
+ if (Length > 2)
 {
     Console.Write ("Третья цифра этого числа - " +b[2]);
 }
