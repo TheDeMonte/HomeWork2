31b539b2601eb4f8120cdf6adbd1b8cc710162a1
Ivan Sidorenko <cidor.0712@gmail.com>
2022-09-20 21:59:08 +0500

    Fixed and added function to task 10

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -1,14 +1,14 @@
 ﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
 
-Console.Write ("Введите трехзначное число: ");
+
+int med (int halfA)
+{
+    int n = halfA / 10;
+    int result = n % 10;
+    return result;
+}
+
+Console.WriteLine ("Введите трезначное число: ");
 int a = Convert.ToInt32(Console.ReadLine());
-string b = Convert.ToString(a);
-int Length = b.Length;
-if (Length >= 4 || Length < 2)
-{
-    Console.WriteLine ("Введено неверное значение");
-}
-else
-{
-    Console.WriteLine ("Вторая цифра этого числа:"+b[1]);
-}
+int Srednee = med (a);
+Console.WriteLine ($"Среднее цифра числа {Srednee}");
