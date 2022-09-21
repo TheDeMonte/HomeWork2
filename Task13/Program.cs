bd96532c30e23511dc1c855b15b617b7c0568d30
Ivan Sidorenko <cidor.0712@gmail.com>
2022-09-21 23:05:54 +0500

    Method for 13 task

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -2,18 +2,18 @@
 
 int ThirdC (int a)
 {
+    while (a > 1000) a = a / 10;
     int result = a % 10;
-    if (a >= 1000)
-    {
-        result = a % 100 / 10;
-    }
     return result;
-    
+    // if (a >= 1000)
+    // {
+    //     result = a % 100 / 10;
+    // }
 }
 Console.Write ("Введите любое число: ");
-int a = Convert.ToInt32(Console.ReadLine());
-int Third = ThirdC (a);
-if (a < 100)
+int b = Convert.ToInt32(Console.ReadLine());
+int Third = ThirdC (b);
+if (b < 100)
 {
     Console.Write ("У этого числа нет третьей цифры");
 }
